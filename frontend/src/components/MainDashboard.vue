0f47c5e69f71c51299d4def8f023513284167f15
frem <alex.efremov.dev@gmail.com>
2017-12-04 22:14:55 +0200

    Add ramda and calculte data for tables with pick function

--- frontend/src/components/MainDashboard.vue
+++ frontend/src/components/MainDashboard.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="main-dashboard">
     <h1>Main Dashboard</h1>
-    <fin-table :titles="firstTableTitles" />
-    <fin-table :titles="secondTableTitles" />
+    <fin-table
+      :titles="firstTableTitles"
+      :tableData="firstTableValues" />
+    <fin-table
+      :titles="secondTableTitles"
+      :tableData="secondTableValues" />
   </div>
 </template>
 
 <script>
+import { pick } from 'ramda';
 import FinTable from '@/components/finTable/FinTable';
 
 export default {
@@ -27,6 +32,15 @@ export default {
       firstTableTitles: ['Revenue', 'Profit', 'Gross Income'],
       secondTableTitles: ['Assets', 'Liabilities'],
     };
+  },
+
+  computed: {
+    firstTableValues() {
+      return pick(this.firstTableTitles, this.tablesData);
+    },
+    secondTableValues() {
+      return pick(this.secondTableTitles, this.tablesData);
+    },
   },
 };
 </script>
