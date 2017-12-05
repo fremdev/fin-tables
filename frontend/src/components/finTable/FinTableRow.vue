d90a0225b54279899687b93ac14f7ecf04e977e4
frem <alex.efremov.dev@gmail.com>
2017-12-05 16:29:06 +0200

    Use new FinTableCell component

--- frontend/src/components/finTable/FinTableRow.vue
+++ frontend/src/components/finTable/FinTableRow.vue
@@ -1,13 +1,26 @@
 <template>
   <tr>
     <th>{{ title }}</th>
-    <td v-for="(year, index) in years" :key="year">{{ values[index] }}</td>
+    <td v-for="(year, index) in years" :key="year">
+      <fin-table-cell
+        :value="values[index] && values[index].value"
+        :currency="values[index] && values[index].currency"
+        :index="index"
+        :title="title" />
+    </td>
   </tr>
 </template>
 
 <script>
+import FinTableCell from './FinTableCell';
+
 export default {
   name: 'fin-table-row',
+
+  components: {
+    FinTableCell,
+  },
+
   props: {
     title: {
       type: String,
