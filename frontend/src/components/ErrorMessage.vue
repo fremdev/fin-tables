452601abe2029ffa7e7316cdbab5a9ebabe52a96
frem <alex.efremov.dev@gmail.com>
2017-12-09 18:32:13 +0200

    Add ErrorMessage component

---
+++ frontend/src/components/ErrorMessage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-if="message" class="error-message">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: 'error-message',
+
+  computed: {
+    ...mapState({
+      message: state => state.messages.error,
+    }),
+  },
+};
+</script>
+
+<style scoped>
+.error-message {
+  color: #721c24;
+  background-color: #f8d7da;
+  border-color: #f5c6cb;
+  border-radius: 5px;
+  padding: .75rem 1.25rem;
+  margin-bottom: 1rem;
+}
+</style>
