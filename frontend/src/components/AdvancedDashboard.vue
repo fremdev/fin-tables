bc75042cc95507695b1fafd54ae1878886a7acab
frem <alex.efremov.dev@gmail.com>
2017-12-09 18:52:18 +0200

    Add Advanced dashboard content

--- frontend/src/components/AdvancedDashboard.vue
+++ frontend/src/components/AdvancedDashboard.vue
@@ -1,17 +1,45 @@
 <template>
   <div class="advanced-dashboard">
     <h1>Advanced Dashboard</h1>
+    <main-controls />
+    <error-message />
+    <fin-table
+      :titles="firstTableTitles"
+      :tableData="firstTableValues" />
   </div>
 </template>
 
 <script>
+import { pick } from 'ramda';
+import FinTable from '@/components/finTable/FinTable';
 import MainControls from '@/components/MainControls';
+import ErrorMessage from '@/components/ErrorMessage';
 
 export default {
   name: 'advanced-dashboard',
 
   components: {
+    FinTable,
     MainControls,
+    ErrorMessage,
+  },
+
+  props: {
+    tablesData: {
+      type: Object,
+    },
+  },
+
+  data() {
+    return {
+      firstTableTitles: ['Price per acre', 'Number acres', 'Input costs'],
+    };
+  },
+
+  computed: {
+    firstTableValues() {
+      return pick(this.firstTableTitles, this.tablesData);
+    },
   },
 };
 </script>
